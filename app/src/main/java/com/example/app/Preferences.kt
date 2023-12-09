7ad2a8f6f22402c158e4367720ec8c961bf7c1c4
wingsmight <wingsmight@gmail.com>
2023-12-09 20:55:20 +0600

    refactor Settings screen

--- app/src/main/java/com/example/app/Preferences.kt
+++ app/src/main/java/com/example/app/Preferences.kt
@@ -3,18 +3,18 @@ package com.example.app
 import androidx.annotation.StringRes
 
 enum class Preferences(
-    @StringRes val leftName:Int, @StringRes val rightName:Int, var withSwitch: Boolean
+    @StringRes val leftName: Int,
+    @StringRes val rightName: Int,
+    var withSwitch: Boolean
 ) {
-   interfaceLanguage(R.string.app_interface_language,R.string.english,false),
-    contentLanguage(R.string.content_language,R.string.russian,false),
-    currency(R.string.currency,R.string.selected_currency,false),
-    darkTheme(R.string.dark_theme,R.string.sys_design,false),
-    adultContentRestrictions(R.string.adult_content,R.string.disabled,false),
+    interfaceLanguage(R.string.app_interface_language, R.string.english, false),
+    contentLanguage(R.string.content_language, R.string.russian, false),
+    currency(R.string.currency, R.string.selected_currency, false),
+    darkTheme(R.string.dark_theme, R.string.sys_design, false),
+    adultContentRestrictions(R.string.adult_content, R.string.disabled, false),
 
-    autoloadOnPlay(R.string.autoload,R.string.without,true),
-    downloadOnlyViaWiFi(R.string.download_only_via_wifi,R.string.without,true),
+    autoloadOnPlay(R.string.autoload, R.string.without, true),
+    downloadOnlyViaWiFi(R.string.download_only_via_wifi, R.string.without, true),
 
-    reportProblem(R.string.report,R.string.without,false)
-
-
+    reportProblem(R.string.report, R.string.without, false)
 }
