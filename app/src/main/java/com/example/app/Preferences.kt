74ee58d56de0ccbb8bf8711c410f8643f6cc9670
Kristina <kpilimonova061@gmail.com>
2023-12-10 20:28:18 +0700

    commit

--- app/src/main/java/com/example/app/Preferences.kt
+++ app/src/main/java/com/example/app/Preferences.kt
@@ -3,14 +3,14 @@ package com.example.app
 import androidx.annotation.StringRes
 
 enum class Preferences(
-    @StringRes val leftName:Int, @StringRes val rightName:Int, var withSwitch: Boolean
+    @StringRes val leftName: Int, @StringRes val rightName: Int,
 ) {
-    InterfaceLanguage(R.string.app_interface_language,R.string.english,false),
-    ContentLanguage(R.string.content_language,R.string.russian,false),
-    Currency(R.string.currency,R.string.selected_currency,false),
-    DarkTheme(R.string.dark_theme,R.string.sys_design,false),
-    AdultContentRestrictions(R.string.adult_content,R.string.disabled,false),
-    AutoloadOnPlay(R.string.autoload,R.string.without,true),
-    DownloadOnlyViaWiFi(R.string.download_only_via_wifi,R.string.without,true),
-    ReportProblem(R.string.report,R.string.without,false)
+    InterfaceLanguage(R.string.app_interface_language, R.string.english),
+    ContentLanguage(R.string.content_language, R.string.russian),
+    Currency(R.string.currency, R.string.selected_currency),
+    DarkTheme(R.string.dark_theme, R.string.sys_design),
+    AdultContentRestrictions(R.string.adult_content, R.string.disabled),
+    AutoloadOnPlay(R.string.autoload, R.string.without),
+    DownloadOnlyViaWiFi(R.string.download_only_via_wifi, R.string.without),
+    ReportProblem(R.string.report, R.string.without)
 }
--- app/src/main/java/com/example/app/ScreenSettings.kt
+++ app/src/main/java/com/example/app/ScreenSettings.kt
@@ -1,7 +1,5 @@
 package com.example.app
 
-
-import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -13,8 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.size
+
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
@@ -29,29 +26,24 @@ import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-
 @Composable
-fun ScreenSettings(@StringRes columnName: Int) {
-    val textSize = 15.sp
-
+fun ScreenSettings(
+    modifier: Modifier = Modifier,
+) {
     val interfaceLanguage = Preferences.InterfaceLanguage
-
     val contentLanguage = Preferences.ContentLanguage
-
     val currency = Preferences.Currency
-
     val darkTheme = Preferences.DarkTheme
-
     val adultContentRestrictions = Preferences.AdultContentRestrictions
-
     val autoloadOnPlay = Preferences.AutoloadOnPlay
-
     val downloadOnlyViaWiFi = Preferences.DownloadOnlyViaWiFi
+    val reportProblem = Preferences.ReportProblem
 
     val rowsWithoutSwitch = listOf(
         interfaceLanguage, contentLanguage,
@@ -59,15 +51,15 @@ fun ScreenSettings(@StringRes columnName: Int) {
         darkTheme,
         adultContentRestrictions,
     )
-
     val rowsWithSwitch = listOf(
         autoloadOnPlay,
         downloadOnlyViaWiFi
     )
 
     Box(
-         Modifier
-            .background(Color.Transparent),
+        Modifier
+            .fillMaxSize()
+            .background(color = colorResource(R.color.light_gray)),
 
         ) {
 
@@ -77,24 +69,10 @@ fun ScreenSettings(@StringRes columnName: Int) {
                 .background(Color.Transparent),
             horizontalAlignment = Alignment.Start,
         ) {
+            ContentName(stringResource(R.string.settings))
 
-            Row(
-                Modifier
-                    .fillMaxWidth()
-                    .height(50.dp)
-                    .padding(start = 15.dp)
-                    .padding(top = 4.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-
-                Text(
-                    stringResource(id = columnName),
-                    fontSize = textSize,
-                )
-            }
             for (row in rowsWithoutSwitch) {
-
-                RowView(
+                TextRow(
                     row
                 )
 
@@ -104,8 +82,7 @@ fun ScreenSettings(@StringRes columnName: Int) {
                 )
             }
             for (row in rowsWithSwitch) {
-
-                RowWithSwitch(
+                SwitchRow(
                     row
                 )
 
@@ -114,112 +91,75 @@ fun ScreenSettings(@StringRes columnName: Int) {
                 )
             }
 
-            Row(
-                modifier = Modifier
-                    .height(3.dp)
-                    .fillMaxWidth()
-                    .background(colorResource(id = R.color.gray))
-                    .padding(top = 2.dp)
-            ) {}
+            Divider(
+                color = colorResource(R.color.gray),
+                thickness = 2.dp
+            )
+            ContentName(stringResource(R.string.report))
+            TextRow(reportProblem)
         }
     }
 }
 
 @Composable
-private fun RowWithSwitch(
-    preference: Preferences,
-    modifier: Modifier = Modifier,
-) {
-    var checkedState by remember { mutableStateOf(false) }
-
-    val textSize = 15.sp
-
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(45.dp)
-            .background(Color.White)
-            .padding(start = 15.dp, end = 2.dp),
-
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-
-        Text(
-
-            stringResource(preference.leftName),
-            fontSize = textSize,
-
-            modifier = Modifier
-
-        )
-
-        Spacer(modifier = Modifier.weight(1.0f))
-
-            Switch(
-                checked = checkedState,
-                onCheckedChange = { checkedState = it }
-            )
-
-            Image(
-                painter = painterResource(id = R.drawable.triangle),
-                contentDescription = "triangle",
-                modifier = Modifier
-                    .padding(horizontal = 13.dp)
-                    .size(10.dp)
-            )
-        }
-    }
-
-
-@Composable
-private fun RowView(
-    preference: Preferences,
-    modifier: Modifier = Modifier,
-) {
-    val textSize = 15.sp
-
+private fun TextRow(preference: Preferences) {
     Row(
         Modifier
-            .safeContentPadding()
-            .fillMaxWidth()
-            .clickable {
+            .clickable { }
+    ) {
+        RowMaker(
+            Modifier,
+            preference,
+            content = {
+                Text(
+                    text = stringResource(id = preference.rightName),
+                    Modifier
+                        .padding(10.dp)
+                        .alpha(0.75f)
+                )
             }
-            .height(45.dp)
-            .background(Color.White)
-            .padding(start = 15.dp, end = 2.dp),
+        )
 
+        Image(painter = painterResource(id = R.drawable.triangle), contentDescription = "triangle")
+    }
+}
+
+@Composable
+private fun SwitchRow(preference: Preferences) {
+    var checkedState by remember { mutableStateOf(false) }
+
+    RowMaker(
+        Modifier,
+        preference,
+        content = { Switch(checked = checkedState, onCheckedChange = { checkedState = it }) }
+    )
+}
+
+@Composable
+private fun ContentName(contentName: String) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+            .padding(start = 15.dp)
+            .padding(top = 4.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
 
         Text(
-
-            stringResource(preference.leftName),
-            fontSize = textSize,
-            modifier = Modifier
-        )
-
-        Spacer(modifier = Modifier.weight(1.0f))
-
-
-        Text(
-            stringResource(id = preference.rightName),
-            modifier = Modifier.alpha(0.7f),
-            fontSize = textSize,
-        )
-
-        Image(
-            painter = painterResource(id = R.drawable.triangle),
-            contentDescription = "triangle",
-            modifier = Modifier
-                .padding(horizontal = 13.dp)
-                .size(10.dp)
+            text = contentName,
+            fontSize = 15.sp,
         )
     }
 }
+
 @Composable
-private fun RowMaker(modifier: Modifier,
-                     preference: Preferences,
-                     content:@Composable() -> Unit){
+private fun RowMaker(
+    modifier: Modifier = Modifier,
+    preference: Preferences,
+    content: @Composable () -> Unit,
+) {
+
 
     Row(
         modifier = Modifier
@@ -227,56 +167,23 @@ private fun RowMaker(modifier: Modifier,
             .height(45.dp)
             .background(Color.White)
             .padding(start = 15.dp, end = 2.dp),
-
         verticalAlignment = Alignment.CenterVertically
     ) {
 
         Text(
-
             stringResource(preference.leftName),
-
+            fontSize = 15.sp,
             modifier = Modifier
-
         )
 
         Spacer(modifier = Modifier.weight(1.0f))
 
-        content
-
-
+        content()
     }
 }
 
 @Preview
 @Composable
-private fun SettingsScreenPreview() {
-    val pref = Preferences.ReportProblem
-
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.light_gray))
-    ) {
-
-        Column(
-            modifier = Modifier.fillMaxSize()
-        ) {
-
-            ScreenSettings(R.string.settings)
-
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(50.dp)
-                    .padding(start = 15.dp)
-                    .padding(top = 4.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-
-                Text(stringResource(id = R.string.support))
-            }
-
-            RowView(preference = pref)
-        }
-    }
+private fun ScreenSettingsPreview() {
+    ScreenSettings()
 }
